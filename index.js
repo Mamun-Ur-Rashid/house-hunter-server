d03d3ab414176787ba32619077a944a71e53d392
Mamun-Ur-Rashid <engmamun656@gmail.com>
2024-01-24 11:45:29 +0600

    add house, all house, housecollect api create

--- index.js
+++ index.js
@@ -31,8 +31,10 @@ async function run() {
     // await client.connect();
 
     const userCollection = client.db('houseHunterDb').collection('users');
+    const houseCollection = client.db('houseHunterDb').collection('houses');
 
  // Register User
+
  app.post('/users', async (req, res) => {
     try {
         const {fullName, role, phoneNumber, email, password} = req.body;
@@ -62,6 +64,70 @@ async function run() {
     }
  });
 
+//  user login 
+
+app.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+    
+    // check if ther user exists
+    const user = await userCollection.findOne({email});
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid User'});
+    }
+    // check if the password is correct
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if(!passwordMatch) {
+        return res.status(401).json({ message: 'Invalid User'});
+    }
+    // generate jwt token
+    const token = jwt.sign({ userId: user._id, email: user.email}, process.env.JWT_Secret, { expiresIn: '2h'});
+    const userInfo = {
+        userId: user._id,
+        email: user.email,
+        fullName: user.fullName,
+        role: user.role,
+        phoneNumber: user.phoneNumber,
+      };
+    res.status(200).json({token, user: userInfo});
+ } catch (error) {
+    console.error(error);
+    res.status(500).json({message: 'Internal server error'});
+ }
+
+});
+
+// house owner 
+app.get('/users/houseOwner/:email', async (req, res) => {
+    
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+
+        const result = { isHouseOwner: user.role === 'House Owner' };
+        return res.json(result);
+   
+});
+
+// house adding api
+app.post('/addHouse', async (req, res) => {
+  const newHouse = req.body;
+  const result = await houseCollection.insertOne(newHouse);
+  res.send(result);
+})
+// get my house
+app.get('/houses/:phoneNumber', async (req, res) => {
+  const Number = req.params.phoneNumber;
+  const query = {phoneNumber: Number};
+  const houses = await houseCollection.find(query).toArray();
+  res.send(houses);
+})
+// get all Houses
+app.get('/allHouses', async( req, res) => {
+  const houses = houseCollection.find().limit(10);
+  const result = await houses.toArray();
+  res.send(result);
+})
     // Send a ping to confirm a successful connection
 
     await client.db("admin").command({ ping: 1 });
